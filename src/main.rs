698da20e6e25a01604ec74400e7521df7180ea3e
Q 🦄 <q@misell.cymru>
2019-11-12 16:37:20 +0000

    time for staging

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,78 @@
 extern crate serde;
 #[macro_use]
 extern crate log;
+#[macro_use]
+extern crate failure;
 
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, middleware, web};
-use dotenv::dotenv;
 use std::io::Read;
 use std::env;
 use futures::compat::Future01CompatExt;
+use std::collections::HashMap;
 
-#[derive(Clone)]
-struct Config {
-    priv_key: Vec<u8>,
+#[derive(Clone, Debug)]
+struct Cert {
+    priv_key: openssl::ec::EcKey<openssl::pkey::Private>,
+    id_key: openssl::rsa::Rsa<openssl::pkey::Private>,
     key_id: String,
     client_secret: String,
 }
 
+#[derive(Clone)]
+struct Config {
+    certs: HashMap<String, Cert>,
+}
+
 fn config() -> Config {
-    dotenv().ok();
-    let cert_path = env::var("APPLE_CERT")
-        .expect("APPLE_CERT must be set");
-    let key_id = env::var("APPLE_CERT_ID")
-        .expect("APPLE_CERT_ID must be set");
-    let client_secret = env::var("CLIENT_SECRET")
-        .expect("CLIENT_SECRET must be set");
+    dotenv::dotenv().ok();
 
-    let mut buf = Vec::new();
+    let file_prefix = "APPLE_CERT_FILE_".to_string();
+    let file_prefix_len = file_prefix.len();
 
-    std::fs::File::open(cert_path).expect("Unable to open apple certificate")
-        .read_to_end(&mut buf).expect("Unable to read apple certificate");
+    let mut certs = HashMap::new();
+
+    for (key, value) in env::vars() {
+        if key.starts_with(&file_prefix) {
+            let client_id = &key[file_prefix_len..];
+            let mut buf = Vec::new();
+            std::fs::File::open(value).expect(&format!("Unable to open {} apple certificate", client_id))
+                .read_to_end(&mut buf).expect(&format!("Unable to read {} apple certificate", client_id));
+
+            let key = openssl::pkey::PKey::private_key_from_pem(&buf)
+                .expect(&format!("{} apple certificate isn't a valid PEM private key", client_id));
+            let ec_key = key.ec_key().expect(&format!("{} apple certificate isn't a ECDSA key", client_id));
+
+            let key_id_key = format!("APPLE_CERT_ID_{}", client_id);
+            let client_secret_key = format!("CLIENT_SECRET_{}", client_id);
+            let id_cert_path_key = format!("ID_CERT_FILE_{}", client_id);
+
+            let key_id = env::var(&key_id_key)
+                .expect(&format!("{} must be set", key_id_key));
+            let client_secret = env::var(&client_secret_key)
+                .expect(&format!("{} must be set", client_secret_key));
+            let id_cert_path = env::var(&id_cert_path_key)
+                .expect(&format!("{} must be set", id_cert_path_key));
+
+            let mut buf = Vec::new();
+            std::fs::File::open(id_cert_path).expect(&format!("Unable to open {} id certificate", client_id))
+                .read_to_end(&mut buf).expect(&format!("Unable to read {} id certificate", client_id));
+
+            let key = openssl::pkey::PKey::private_key_from_pem(&buf)
+                .expect(&format!("{} id certificate isn't a valid PEM private key", client_id));
+            let id_key = key.rsa().expect(&format!("{} id certificate isn't a RSA key", client_id));
+
+            let cert = Cert {
+                priv_key: ec_key,
+                id_key,
+                key_id,
+                client_secret,
+            };
+            certs.insert(client_id.to_string(), cert);
+        }
+    }
 
     Config {
-        priv_key: buf,
-        client_secret,
-        key_id
+        certs
     }
 }
 
@@ -41,25 +81,27 @@ fn config() -> Config {
 struct OauthState {
     redirect_url: String,
     orig_state: Option<String>,
+    client_id: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Clone, Serialize, Deserialize, Debug)]
 struct AppleUserName {
     #[serde(rename = "firstName")]
     first_name: String,
     #[serde(rename = "lastName")]
-    last_name: String
-}
-#[derive(Serialize, Deserialize, Debug)]
-struct AppleUserInfo {
-    name: AppleUserName,
-    email: String
+    last_name: String,
 }
 
-#[derive(Serialize, Deserialize)]
-struct OauthCode<'a> {
-    user_info: Option<&'a AppleUserInfo>,
-    orig_code: String
+#[derive(Clone, Serialize, Deserialize, Debug)]
+struct AppleUserInfo {
+    name: AppleUserName,
+    email: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct OauthCode {
+    user_info: Option<AppleUserInfo>,
+    orig_code: String,
 }
 
 #[derive(Deserialize)]
@@ -68,7 +110,7 @@ struct OauthLoginInfo {
     redirect_uri: String,
     scope: Option<String>,
     nonce: Option<String>,
-    state: Option<String>
+    state: Option<String>,
 }
 
 async fn start_login(req: HttpRequest, info: web::Query<OauthLoginInfo>) -> actix_web::Result<impl actix_web::Responder> {
@@ -86,8 +128,9 @@ async fn start_login(req: HttpRequest, info: web::Query<OauthLoginInfo>) -> acti
 
     let state = serde_json::to_string(&OauthState {
         redirect_url: info.redirect_uri.clone(),
-        orig_state: info.state.clone()
-    }).unwrap();
+        orig_state: info.state.clone(),
+        client_id: info.client_id.clone()
+    })?;
     redirect_uri.push_str(&format!("&state={}", base64::encode(&state)));
 
     Ok(
@@ -102,26 +145,38 @@ struct OauthCallbackInfo {
     state: String,
     code: Option<String>,
     error: Option<String>,
-    user: Option<AppleUserInfo>
+    user: Option<String>,
 }
 
-
-async fn finish_login(info: web::Form<OauthCallbackInfo>) -> actix_web::Result<impl actix_web::Responder> {
-    println!("{:?}",&info.state);
-    let state: OauthState = serde_json::from_slice(&base64::decode(&info.state).unwrap()).unwrap();
+async fn finish_login(info: web::Form<OauthCallbackInfo>, data: web::Data<Config>) -> failure::Fallible<impl actix_web::Responder> {
+    let state: OauthState = serde_json::from_slice(&base64::decode(&info.state)?)?;
     let mut redirect_uri = state.redirect_url;
 
     if let Some(error) = &info.error {
         redirect_uri.push_str(&format!("?error={}", error))
     } else {
         let code = info.code.clone().unwrap_or("".to_string());
-        let new_code = base64::encode(&serde_json::to_string(&OauthCode {
+        let user: Option<AppleUserInfo> = match info.user {
+            Some(ref u) => Some(serde_json::from_str(u)?),
+            None => None
+        };
+
+        let cert = match data.certs.get(&state.client_id) {
+            Some(c) => c,
+            None => bail!("Client ID not found")
+        };
+
+        let header = AppleSecretHeader {
+            alg: "RS256".to_string(),
+            kid: state.client_id.clone(),
+        };
+        let new_code = encode_jwt_rsa(&header, &OauthCode {
             orig_code: code,
-            user_info: info.user.as_ref()
-        }).unwrap());
+            user_info: user,
+        }, &cert.id_key)?;
         redirect_uri.push_str(&format!("?code={}", &new_code));
         if let Some(state) = &state.orig_state {
-            redirect_uri.push_str(&format!("&state={}",state));
+            redirect_uri.push_str(&format!("&state={}", state));
         }
     }
 
@@ -140,7 +195,13 @@ struct OauthTokenInfo {
     code: Option<String>,
     grant_type: String,
     refresh_token: Option<String>,
-    redirect_uri: Option<String>
+    redirect_uri: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct AppleSecretHeader {
+    alg: String,
+    kid: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -152,31 +213,121 @@ struct AppleSecretClaims {
     sub: String,
 }
 
-async fn get_token(req: HttpRequest, data: web::Data<Config>, info: web::Form<OauthTokenInfo>) -> actix_web::Result<impl actix_web::Responder> {
-    if info.client_secret != data.client_secret {
+
+#[derive(Debug, Serialize, Deserialize)]
+struct TokenResponse {
+    access_token: String,
+    expires_in: u64,
+    id_token: String,
+    refresh_token: String,
+    token_type: String,
+}
+
+fn encode_jwt_ecdsa<H: serde::Serialize, C: serde::Serialize>(header: &H, claims: &C, priv_key: &openssl::ec::EcKeyRef<openssl::pkey::Private>) -> failure::Fallible<String> {
+    let header_str = base64::encode_config(&serde_json::to_string(header)?, base64::URL_SAFE_NO_PAD);
+    let claims_str = base64::encode_config(&serde_json::to_string(claims)?, base64::URL_SAFE_NO_PAD);
+
+    let mut secret = String::new();
+    secret.push_str(&header_str);
+    secret.push('.');
+    secret.push_str(&claims_str);
+
+    let mut digester = openssl::sha::Sha256::new();
+    digester.update(&secret.as_bytes());
+    let signer = openssl::ecdsa::EcdsaSig::sign(&digester.finish(), priv_key)?;
+    let mut signature = signer.r().to_vec();
+    signature.extend(signer.s().to_vec());
+    let signature = base64::encode_config(&signature, base64::URL_SAFE_NO_PAD);
+
+    secret.push('.');
+    secret.push_str(&signature);
+
+    Ok(secret)
+}
+
+fn encode_jwt_rsa<H: serde::Serialize, C: serde::Serialize>(header: &H, claims: &C, priv_key: &openssl::rsa::RsaRef<openssl::pkey::Private>) -> failure::Fallible<String> {
+    let header_str = base64::encode_config(&serde_json::to_string(header)?, base64::URL_SAFE_NO_PAD);
+    let claims_str = base64::encode_config(&serde_json::to_string(claims)?, base64::URL_SAFE_NO_PAD);
+
+    let mut secret = String::new();
+    secret.push_str(&header_str);
+    secret.push('.');
+    secret.push_str(&claims_str);
+
+    let pkey = openssl::pkey::PKey::from_rsa(priv_key.to_owned())?;
+    let mut signer = openssl::sign::Signer::new(openssl::hash::MessageDigest::sha256(), &pkey)?;
+    signer.set_rsa_padding(openssl::rsa::Padding::PKCS1)?;
+    signer.update(&secret.as_bytes())?;
+    let signature = base64::encode_config(&signer.sign_to_vec()?, base64::URL_SAFE_NO_PAD);
+
+    secret.push('.');
+    secret.push_str(&signature);
+
+    Ok(secret)
+}
+
+async fn get_apple_keys() -> failure::Fallible<JWKSet> {
+    let client = reqwest::r#async::Client::new();
+    Ok(client
+        .get("https://appleid.apple.com/auth/keys")
+        .send()
+        .compat()
+        .await?
+        .json::<JWKSet>()
+        .compat()
+        .await?)
+}
+
+async fn get_token(req: HttpRequest, data: web::Data<Config>, info: web::Form<OauthTokenInfo>) -> failure::Fallible<impl actix_web::Responder> {
+    let cert = match data.certs.get(&info.client_id) {
+        Some(c) => c,
+        None => bail!("Client ID not found")
+    };
+
+    if info.client_secret != cert.client_secret {
         return Ok(HttpResponse::Forbidden().finish());
     }
 
-    let mut header = jsonwebtoken::Header::default();
-    header.alg = jsonwebtoken::Algorithm::ES256;
-    header.kid = Some(data.key_id.clone());
+    let header = AppleSecretHeader {
+        alg: "ES256".to_string(),
+        kid: cert.key_id.clone(),
+    };
     let now = std::time::SystemTime::now();
-    let exp = now + std::time::Duration::new(5*60, 0);
+    let exp = now + std::time::Duration::new(5 * 60, 0);
     let claims = AppleSecretClaims {
         iss: "MQ9TN9772U".to_string(),
-        iat: now.duration_since(std::time::SystemTime::UNIX_EPOCH).unwrap().as_secs(),
-        exp: exp.duration_since(std::time::SystemTime::UNIX_EPOCH).unwrap().as_secs(),
+        iat: now.duration_since(std::time::SystemTime::UNIX_EPOCH)?.as_secs(),
+        exp: exp.duration_since(std::time::SystemTime::UNIX_EPOCH)?.as_secs(),
         aud: "https://appleid.apple.com".to_string(),
-        sub: info.client_id.clone()
+        sub: info.client_id.clone(),
     };
-    let secret = jsonwebtoken::encode(&header, &claims, &data.priv_key).unwrap();
+
+    let secret = encode_jwt_ecdsa(&header, &claims, &cert.priv_key)?;
 
     let code: Option<OauthCode> = match &info.code {
-        Some(code) => Some(serde_json::from_slice(&base64::decode(&code).unwrap()).unwrap()),
+        Some(code) => {
+            let parts: Vec<&str> = code.split(".").collect();
+            ensure!(parts.len() == 3);
+            let header: AppleSecretHeader = serde_json::from_slice(
+                &base64::decode_config(parts[0], base64::URL_SAFE_NO_PAD)?
+            )?;
+            ensure!(&header.kid == &info.client_id);
+            let pkey = openssl::pkey::PKey::from_rsa(cert.id_key.clone())?;
+
+            let sig = base64::decode_config(parts[2], base64::URL_SAFE_NO_PAD)?;
+            let mut verifier = openssl::sign::Verifier::new(openssl::hash::MessageDigest::sha256(), &pkey)?;
+            verifier.set_rsa_padding(openssl::rsa::Padding::PKCS1)?;
+            verifier.update(parts[0].as_bytes())?;
+            verifier.update(".".as_bytes())?;
+            verifier.update(parts[1].as_bytes())?;
+            ensure!(verifier.verify(&sig)?);
+
+            Some(serde_json::from_slice(&base64::decode_config(parts[1], base64::URL_SAFE_NO_PAD)?)?)
+        },
         None => None
     };
-    let orig_code = match code {
-        Some(code) => Some(code.orig_code),
+    let orig_code = match &code {
+        Some(code) => Some(code.orig_code.clone()),
         None => None
     };
 
@@ -189,15 +340,124 @@ async fn get_token(req: HttpRequest, data: web::Data<Config>, info: web::Form<Oa
             code: orig_code,
             grant_type: info.grant_type.clone(),
             refresh_token: info.refresh_token.clone(),
-            redirect_uri: Some(own_uri)
+            redirect_uri: Some(own_uri),
         })
-        .send().compat().await.unwrap();
+        .send().compat().await?;
 
-    let text = resp.text().compat().await.unwrap();
+    let token = resp.json::<TokenResponse>().compat().await?;
+
+    let parts: Vec<&str> = token.id_token.split(".").collect();
+    ensure!(parts.len() == 3);
+    let header: AppleSecretHeader = serde_json::from_slice(
+        &base64::decode_config(parts[0], base64::URL_SAFE_NO_PAD)?
+    )?;
+    let keys = get_apple_keys().await?;
+    let key = match keys.get_key(&header.kid) {
+        Some(k) => k,
+        None => bail!("Apple signing key not found")
+    };
+
+    match key.kty.as_str() {
+        "RSA" => {
+            let e = base64::decode_config(&key.e, base64::URL_SAFE_NO_PAD)?;
+            let n = base64::decode_config(&key.n, base64::URL_SAFE_NO_PAD)?;
+
+            let e = openssl::bn::BigNum::from_slice(&e)?;
+            let n = openssl::bn::BigNum::from_slice(&n)?;
+
+            let key = openssl::rsa::Rsa::from_public_components(n, e)?;
+            let pkey = openssl::pkey::PKey::from_rsa(key)?;
+
+            let sig = base64::decode_config(parts[2], base64::URL_SAFE_NO_PAD)?;
+            let mut verifier = openssl::sign::Verifier::new(openssl::hash::MessageDigest::sha256(), &pkey)?;
+            verifier.set_rsa_padding(openssl::rsa::Padding::PKCS1)?;
+            verifier.update(parts[0].as_bytes())?;
+            verifier.update(".".as_bytes())?;
+            verifier.update(parts[1].as_bytes())?;
+            ensure!(verifier.verify(&sig)?);
+        }
+        _ => bail!("Apple ID token should be using RSA")
+    };
+
+    let new_header = AppleSecretHeader {
+        alg: "RS256".to_string(),
+        kid: info.client_id.clone(),
+    };
+    let mut claims: serde_json::Map<String, serde_json::Value> = serde_json::from_slice(
+        &base64::decode_config(parts[1], base64::URL_SAFE_NO_PAD)?
+    )?;
+
+    if let Some(code) = code {
+        if let Some(user_info) = code.user_info {
+            claims.insert("given_name".to_string(), serde_json::value::Value::String(user_info.name.first_name));
+            claims.insert("family_name".to_string(), serde_json::value::Value::String(user_info.name.last_name));
+        }
+    }
+
+    let new_token = encode_jwt_rsa(&new_header, &claims, &cert.id_key)?;
+
+    let new_token = TokenResponse {
+        access_token: token.access_token,
+        expires_in: token.expires_in,
+        refresh_token: token.refresh_token,
+        token_type: token.token_type,
+        id_token: new_token,
+    };
 
     Ok(
         HttpResponse::Ok()
-            .body(text)
+            .json(new_token)
+    )
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct JWK {
+    kty: String,
+    r#use: String,
+    alg: String,
+    kid: String,
+    n: String,
+    e: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct JWKSet {
+    keys: Vec<JWK>
+}
+
+impl JWKSet {
+    fn get_key(&self, kid: &str) -> Option<&JWK> {
+        for key in self.keys.iter() {
+            if key.kid == kid {
+                return Some(key);
+            }
+        }
+        None
+    }
+}
+
+async fn jwks(data: web::Data<Config>) -> actix_web::Result<impl actix_web::Responder> {
+    let keys: Vec<_> = data.certs.iter()
+        .map(|(client_id, cert)| {
+            let n = cert.id_key.n().to_vec();
+            let e = cert.id_key.e().to_vec();
+
+            JWK {
+                kty: "RSA".to_string(),
+                r#use: "sig".to_string(),
+                alg: "RSA256".to_string(),
+                kid: client_id.to_string(),
+                n: base64::encode_config(&n, base64::URL_SAFE_NO_PAD),
+                e: base64::encode_config(&e, base64::URL_SAFE_NO_PAD),
+            }
+        })
+        .collect();
+
+    Ok(
+        HttpResponse::Ok()
+            .json(&JWKSet {
+                keys
+            })
     )
 }
 
@@ -216,8 +476,9 @@ fn main() {
             .route(".well-known/apple-developer-domain-association.txt", web::get().to(|| HttpResponse::Ok().body(
                 actix_web::dev::Body::from_slice(include_bytes!("../apple-developer-domain-association.txt")))))
             .route("/auth/authorize", web::get().to_async(actix_web_async_await::compat2(start_login)))
-            .route("/auth/callback", web::post().to_async(actix_web_async_await::compat(finish_login)))
+            .route("/auth/callback", web::post().to_async(actix_web_async_await::compat2(finish_login)))
             .route("/auth/token", web::post().to_async(actix_web_async_await::compat3(get_token)))
+            .route("/auth/keys", web::get().to_async(actix_web_async_await::compat(jwks)))
     });
 
     let mut listenfd = listenfd::ListenFd::from_env();
