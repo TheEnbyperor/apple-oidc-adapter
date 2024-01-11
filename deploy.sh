be01b877ae7e4131970365a10cf1ab0d7c33970f
Q <q@misell.cymru>
2024-01-11 12:50:00 +0000

    fix image name

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 VERSION=$(sentry-cli releases propose-version || exit)
 
-docker build -t "theenbyperor/wwfypc-apple-oidc:$VERSION" . || exit
-docker push "theenbyperor/wwfypc-apple-oidc:$VERSION" || exit
+docker build -t "theenbyperor/as207960-apple-oidc:$VERSION" . || exit
+docker push "theenbyperor/as207960-apple-oidc:$VERSION" || exit
 
 sed -e "s/(version)/$VERSION/g" < deploy.yaml | kubectl apply -f - || exit
